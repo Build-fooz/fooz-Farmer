2b14d7d42cfb88d1a5b38e8778fdc4cf5e70beb2
Tarang Patil <tarangpatil09@gmail.com>
2025-02-21 16:44:19 +0530

    Create Header component. Add prettier-tailwind plugin

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex h-20 items-center justify-between px-10">
+      <img src="/logo.png" alt="Fooz logo" className="h-12 w-auto" />
+      <nav>
+        <a className="mx-4" href="">
+          About us
+        </a>
+        <a className="mx-4" href="">
+          Products
+        </a>
+        <a className="mx-4" href="">
+          Testimonials
+        </a>
+        <a className="mx-4" href="">
+          Contact
+        </a>
+        <button className="ml-6 rounded bg-black px-6 py-2 text-white">
+          Join now
+        </button>
+      </nav>
+    </header>
+  );
+}
