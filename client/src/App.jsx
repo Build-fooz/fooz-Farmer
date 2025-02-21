437b9883b31718f94feebef84899e763dc77da8a
Tarang Patil <tarangpatil09@gmail.com>
2025-02-21 17:03:48 +0530

    Create SourcesProducts component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SourcesProducts from "./components/SourcesProducts";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main className="bg-gray-50">
         <Hero />
+        <SourcesProducts />
       </main>
     </>
   );
---
+++ client/src/components/SourcesProducts.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function SourcesProducts() {
+  return (
+    <section className="py-10">
+      <div className="mb-6 text-center text-3xl font-bold">
+        Products We Source
+      </div>
+      <div className="grid grid-cols-4">
+        <div className="m-4 flex flex-col items-center rounded-xl border-2 border-gray-200 bg-white py-10">
+          <img src="/plant-fill.svg" alt="Plant icon" className="h-10" />
+          <p className="text-center text-xl font-bold">Organic Products</p>
+          <p className="text-center text-gray-500">
+            Fresh vegetables, fruits and grains
+          </p>
+        </div>
+        <div className="m-4 flex flex-col items-center rounded-xl border-2 border-gray-200 bg-white py-10">
+          <img src="/honey.svg" alt="Honey jar icon" className="h-10" />
+          <p className="text-center text-xl font-bold">Honey</p>
+          <p className="text-center text-gray-500">
+            Pure and natural honey products
+          </p>
+        </div>
+        <div className="m-4 flex flex-col items-center rounded-xl border-2 border-gray-200 bg-white py-10">
+          <img src="/chilli.svg" alt="Chilli icon" className="h-10" />
+          <p className="text-center text-xl font-bold">Spices</p>
+          <p className="text-center text-gray-500">
+            Fresh vegetables, fruits and grains
+          </p>
+        </div>
+        <div className="m-4 flex flex-col items-center rounded-xl border-2 border-gray-200 bg-white py-10">
+          <img src="/coffee.svg" alt="Coffee icon" className="h-10" />
+          <p className="text-center text-xl font-bold">Coffee</p>
+          <p className="text-center text-gray-500">Specialty coffee beans</p>
+        </div>
+      </div>
+    </section>
+  );
+}
