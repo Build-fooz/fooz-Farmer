c9b490b27db31fdd910bda69ad83c8869c3bbf74
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-02-22 18:08:44 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import Dashboard from './Pages/Dashboard'
 import DeliveryDashboard from './Pages/DeliveryDashboard'
+import NotFound from './Pages/NotFound'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-    <DeliveryDashboard/>
-    <Dashboard/>
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} /> */}
+        <Route path="/userdashboard" element= {<Dashboard/>} />
+        <Route path="/deliverydashboard" element= {<DeliveryDashboard/>} />
+        <Route path="*" element={<NotFound />} /> {/* Handles unknown routes */}
+      </Routes>
+    </Router>
     
   )
 }
---
+++ client/src/Pages/NotFound.jsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+    return(
+        <div>Page not Found! please check URL</div>
+    )
+    
+}
+
+export default NotFound
