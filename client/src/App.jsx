815f01602033fa2a210e6b74b66c40bdb2ec7562
ZAID IQBAL <zaidiqbal171717@gmail.com>
2025-02-26 12:01:58 +0530

    Save changes before switching branches

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,49 @@
-// import { useState } from 'react'
-import './App.css'
-import Dashboard from './Pages/Dashboard'
-import DeliveryDashboard from './Pages/DeliveryDashboard'
-import ProductListing from './Pages/ProductListing'
-import NotFound from './Pages/NotFound'
+// // import { useState } from 'react'
+// import './App.css'
+// import Dashboard from './Pages/Dashboard'
+// import DeliveryDashboard from './Pages/DeliveryDashboard'
+// import ProductListing from './Pages/ProductListing'
+// import NotFound from './Pages/NotFound'
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// function App() {
+
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} /> */}
+//         <Route path="/userdashboard" element= {<Dashboard/>} />
+//         <Route path="/deliverydashboard" element= {<DeliveryDashboard/>} />
+//         <Route path="/productListing" element= {<ProductListing/>} />
+//         <Route path="*" element={<NotFound />} /> {/* Handles unknown routes */}
+//       </Routes>
+//     </Router>
+    
+//   )
+// }
+
+// export default App
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthForm from "./Pages/AuthForm";
+import RegisterForm from "./Pages/RegisterForm";
 
 function App() {
-
   return (
-    <Router>
+
+
+      <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} /> */}
-        <Route path="/userdashboard" element= {<Dashboard/>} />
-        <Route path="/deliverydashboard" element= {<DeliveryDashboard/>} />
-        <Route path="/productListing" element= {<ProductListing/>} />
-        <Route path="*" element={<NotFound />} /> {/* Handles unknown routes */}
+        <Route path="/Register" element={<RegisterForm />} />
+        <Route path="/" element={<AuthForm />} />
       </Routes>
     </Router>
-    
-  )
+
+
+
+  );
 }
 
-export default App
+export default App;
