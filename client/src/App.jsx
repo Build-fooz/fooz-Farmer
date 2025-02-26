b881ad53d4ce2baabffd6c0093d5690d5562324d
ZAID IQBAL <117713986+zaidiqbal17@users.noreply.github.com>
2025-02-26 20:52:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,4 @@
-// // import { useState } from 'react'
-// import './App.css'
-// import Dashboard from './Pages/Dashboard'
-// import DeliveryDashboard from './Pages/DeliveryDashboard'
-// import ProductListing from './Pages/ProductListing'
-// import NotFound from './Pages/NotFound'
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// function App() {
-
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} /> */}
-//         <Route path="/userdashboard" element= {<Dashboard/>} />
-//         <Route path="/deliverydashboard" element= {<DeliveryDashboard/>} />
-//         <Route path="/productListing" element= {<ProductListing/>} />
-//         <Route path="*" element={<NotFound />} /> {/* Handles unknown routes */}
-//       </Routes>
-//     </Router>
-    
-//   )
-// }
-
-// export default App
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
