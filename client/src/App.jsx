45dfda9d3901b87198439559cdb2a6293614a05e
ZAID IQBAL <zaidiqbal171717@gmail.com>
2025-03-03 22:34:54 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
-
-
-
+import './App.css'
+import Dashboard from './Pages/Dashboard'
+import DeliveryDashboard from './Pages/DeliveryDashboard'
+// import Home from './Pages/HomePage'
+import ProductListing from './Pages/ProductListing'
+import NotFound from './Pages/NotFound'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthForm from "./Pages/AuthForm";
 import RegisterForm from "./Pages/RegisterForm";
 
 function App() {
   return (
-
-
-      <Router>
+    <Router>
       <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/userdashboard" element= {<Dashboard/>} />
+        <Route path="/deliverydashboard" element= {<DeliveryDashboard/>} />
+        <Route path="/productListing" element= {<ProductListing/>} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/" element={<AuthForm />} />
+        <Route path="*" element={<NotFound />} /> {/* Handles unknown routes */}
       </Routes>
     </Router>
-
-
-
   );
 }
 
