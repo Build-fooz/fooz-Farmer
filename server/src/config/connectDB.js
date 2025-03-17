3c8a94b980d38c0dab1a88a0c8f0a04460d509a1
Tushar Kumar <160735794+ProjectTusharKumar@users.noreply.github.com>
2025-03-17 18:41:19 +0530

    Update connectDB.js

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -1,19 +1,1 @@
-const mongoose = require("mongoose");
 
-const MONGODB_URI =//past you mongodb url
- 
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected...");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit process with failure
-  }
-};
-
-module.exports = connectDB;
